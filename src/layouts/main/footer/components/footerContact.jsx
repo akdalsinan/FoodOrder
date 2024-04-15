c2db0474529cb4b0797e0f77074f4e1b0303411d
Sinan Akdal <snnakdal@gmail.com>
2024-04-15 14:07:04 +0300

    ContactLinks delete

--- src/layouts/main/footer/components/footerContact.jsx
+++ src/layouts/main/footer/components/footerContact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaLocationDot } from "react-icons/fa6";
 import { IoCallSharp, IoMail } from "react-icons/io5";
-import ContactLinks from "./ContactLinks";
+// import ContactLinks from "./ContactLinks";
 
 function FooterContact() {
   return (
@@ -11,9 +11,9 @@ function FooterContact() {
       <h4 className="font-dancing mx-auto py-1 text-2xl ">Contact</h4>
 
       <div>
-        <ContactLinks name={"location"} icon={<FaLocationDot />} />
+        {/* <ContactLinks name={"location"} icon={<FaLocationDot />} />
         <ContactLinks name={"555 555 07 55"} icon={<IoCallSharp />} />
-        <ContactLinks name={"demo@gmail.com"} icon={<IoMail />} />
+        <ContactLinks name={"demo@gmail.com"} icon={<IoMail />} /> */}
       </div>
     </div>
   );
