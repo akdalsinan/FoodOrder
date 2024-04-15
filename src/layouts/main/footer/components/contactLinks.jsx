b979da8913dedc429d8ed3a92874dfcefdd07a9c
Sinan Akdal <snnakdal@gmail.com>
2024-04-15 11:14:18 +0300

    update

--- src/layouts/main/footer/components/contactLinks.jsx
+++ src/layouts/main/footer/components/contactLinks.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-function ContactLinks({name,icon}) {
+function ContactLinks({ name, icon }) {
   return (
     <div className="location flex flex-wrap">
-      <div className="mt-1 mr-2">
-        {icon} 
-      </div>
+      <div className="mt-1 mr-2">{icon}</div>
       <a className="font-sans hover:text-red-800 cursor-pointer " href="">
         {name}
       </a>
